62e84346428bac01d5918ecb4f55db91829b0788
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-04-04 14:41:26 +0200

    Přidejte soubory projektu.

---
+++ Ct1300_Klikacka/Models/Bunka.cs
@@ -0,0 +1,17 @@
+﻿namespace Ct1300_Klikacka.Models
+{
+    public class Bunka
+    {
+        public Bunka(int hodnota, int radka, int sloupec) 
+        { 
+            Hodnota = hodnota;
+            Radka = radka;
+            Sloupec = sloupec;
+        }
+        public int Hodnota { get; private set; }
+        public int Radka { get; private set; }
+        public int Sloupec { get; private set; }
+        public int StyleClassIndex { get; set; } = 0;
+        public string StyleClassName => $"barva-{StyleClassIndex}";
+    }
+}
---
+++ Ct1300_Klikacka/Models/Grafika.cs
@@ -0,0 +1,37 @@
+﻿namespace Ct1300_Klikacka.Models
+{
+    public class Grafika
+    {
+        public Grafika()
+        {
+            NahodnePole(radku: 8, sloupcu: 10, cisloOd: 0, cisloDo: 1);
+        }
+        public Bunka[,] Pole { get; private set; }
+
+        private int ZvolenaBarva { get; set; } = 2;
+
+        private Random rnd = new Random();
+
+        private void NahodnePole(int radku, int sloupcu, int cisloOd, int cisloDo)
+        {
+            Pole = new Bunka[radku, sloupcu];
+            for (int i = 0; i < radku; i++)
+            {
+                for (int j = 0; j < sloupcu; j++)
+                {
+                    Pole[i, j] = new Bunka(rnd.Next(cisloOd, cisloDo + 1), i, j);
+                }
+            }
+        }
+
+        public void OnCellClick(Models.Bunka bunka)
+        {
+            if (bunka.Hodnota == 0 && bunka.StyleClassIndex != ZvolenaBarva)
+            {
+                bunka.StyleClassIndex = ZvolenaBarva;
+            }
+            else
+                bunka.StyleClassIndex = 0;
+        }
+    }
+}
